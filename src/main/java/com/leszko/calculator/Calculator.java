2af15c5e821f0e6a5c77a803c539778146b980e8
Johnny Zgombic <54005020+jzgombic@users.noreply.github.com>
2022-04-02 22:51:38 -0400

    Update Calculator.java

--- src/main/java/com/leszko/calculator/Calculator.java
+++ src/main/java/com/leszko/calculator/Calculator.java
@@ -3,8 +3,19 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+
+/**
+* Checkstyle I
+*/
 @Service
+/**
+* Checkstyle I
+*/
 public class Calculator {
+	
+/**
+* Checkstyle I
+*/
         final static int umlNUMBER1 = 3;
 	@Cacheable("sum")
 	public int sum(int a, int b) {
